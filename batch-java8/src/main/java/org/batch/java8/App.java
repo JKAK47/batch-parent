6c1192f7561c62d81c46d9497e5f6eeb59ec96a9
JKAK47 <JKAK47@users.noreply.github.com>
2017-09-20 18:48:19 +0800

    commit

--- batch-java8/src/main/java/org/batch/java8/App.java
+++ batch-java8/src/main/java/org/batch/java8/App.java
@@ -1,11 +1,16 @@
 package org.batch.java8;
 
+import org.batch.java8.functioninterface.SpringImpl;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
+		/**
+		 * 调用函数式接口
+		 */
 		SpringImpl impl = new SpringImpl();
 		System.out.println(impl.getName());
 		System.out.println(impl.Search());
---
+++ batch-java8/src/main/java/org/batch/java8/functioninterface/SpringImpl.java
@@ -0,0 +1,11 @@
+package org.batch.java8.functioninterface;
+
+public class SpringImpl implements Spring {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "Vincent";
+	}
+
+}
