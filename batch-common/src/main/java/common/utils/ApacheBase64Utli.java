beb0f25445fcd7970dae5afe42419fb04779e230
PR <1557862201@qq.com>
2017-10-30 08:11:22 +0800

    工具类Base64 md5
    工具类Base64 md5

---
+++ batch-common/src/main/java/common/utils/ApacheBase64Utli.java
@@ -0,0 +1,81 @@
+package common.utils;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.util.StringUtils;
+
+public class ApacheBase64Utli {
+
+	public ApacheBase64Utli() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * Apache 的Base64 编码方法
+	 *
+	 * @Title: Base64Encode
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @param text
+	 *            需要Base64编码的字符串
+	 * @param charsetName
+	 *            字符串编码为字节数组的编码字符集
+	 * @return String 返回base64编码后的字符串
+	 * @throws
+	 */
+	public static String Base64Encode(String text, String charsetName) {
+		Base64 base64 = new Base64();
+		String result = null;
+		if (!StringUtils.isEmpty(text)) {
+			try {
+				if (!StringUtils.isEmpty(charsetName)) {
+					result = base64.encodeToString(text.getBytes(charsetName));
+				} else {
+					result = base64.encodeToString(text.getBytes("utf-8"));
+				}
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public static String Base64Decode(String text, String charsetName) {
+		Base64 base64 = new Base64();
+		// 解码Base64
+		byte[] debytes = base64.decode(text);
+		// System.out.println(Arrays.toString(debytes));
+		// 还原字符串
+		String result = null;
+
+		try {
+			if (!StringUtils.isEmpty(charsetName)) {
+				result = new String(debytes, charsetName);
+			} else {
+				result = new String(debytes, "utf-8");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return result;
+
+	}
+
+	public static void main(String[] args) {
+		// apache base64 编码测试案例
+		System.out.println(ApacheBase64Utli.Base64Encode("中国", null));
+		System.out.println(ApacheBase64Utli.Base64Encode("中国", "utf-8"));
+		System.out.println(ApacheBase64Utli.Base64Encode("中国", "ASCII"));
+		System.out.println(ApacheBase64Utli.Base64Encode("中国", "GBK"));
+		// base64 解码测试案例,用UTF-8 编码字符串为字节数组后然后再次用Base64进行编码
+		String encode = ApacheBase64Utli.Base64Encode("中国", "utf-8");
+		System.out.println(ApacheBase64Utli.Base64Decode(encode, null));
+		System.out.println(ApacheBase64Utli.Base64Decode(encode, "utf-8"));
+		/**
+		 * 下面解码会错误，当base64 解码后再次用GBK将字节数组解码为字符串时候出错， 原来字符串使用utf-8编码为字节数组
+		 */
+		System.out.println(ApacheBase64Utli.Base64Decode(encode, "GBK"));
+	}
+}
---
+++ batch-common/src/main/java/common/utils/ConverterUtils.java
@@ -0,0 +1,93 @@
+package common.utils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.springframework.util.StringUtils;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class ConverterUtils {
+
+	public ConverterUtils() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 *
+	 * @Title: Base64Encode 编码
+	 * @Description: TODO(Base64Encode 编码工具方法)
+	 * @param text
+	 *            待编码的字符串
+	 * @param charsetName
+	 *            待编码字符串转换为字节的字符编码集，默认是UTT-8
+	 * @throws UnsupportedEncodingException
+	 *             设定文件
+	 * @return String 返回Base64编码后的字符串
+	 * @throws
+	 */
+	public static String Base64Encode(String text, String charsetName)
+			throws UnsupportedEncodingException {
+		BASE64Encoder encoder = new BASE64Encoder();
+		String result = null;
+
+		if (!StringUtils.isEmpty(text)) {
+			if (StringUtils.isEmpty(charsetName)) {
+				result = encoder.encode(text.getBytes("utf-8"));
+			} else {
+				result = encoder.encode(text.getBytes(charsetName));
+			}
+		}
+		return result;
+	}
+
+	/**
+	 *
+	 * @Title: Base64Decode
+	 * @Description: TODO(Base64 解码工具方法 )
+	 * @param decodeText
+	 *            待解码的Base64 编码字符串
+	 * @param charsetName
+	 *            Base64解码后组装字符串时使用的字符集，默认使用UTF-8
+	 * @param
+	 * @param IOException
+	 *            设定文件
+	 * @return String 返回类型
+	 * @throws
+	 */
+	public static String Base64Decode(String decodeText, String charsetName)
+			throws IOException {
+		BASE64Decoder decoder = new BASE64Decoder();
+		byte[] bytes = null;
+		String result = null;
+		if (!StringUtils.isEmpty(decodeText)) {
+			bytes = decoder.decodeBuffer(decodeText);
+		}
+		if (!StringUtils.isEmpty(charsetName)) {
+			result = new String(bytes, charsetName);
+		} else {
+			result = new String(bytes, "utf-8");
+		}
+		return result;
+
+	}
+
+	public static String ByMD5() {
+		return null;
+	}
+
+	public static void main(String[] args) throws IOException {
+		byte[] bytes = "A".getBytes("utf-8");
+		for (int i = 0; i < bytes.length; i++) {
+			System.out.println(bytes[i]);
+		}
+		System.out.println(Integer.toHexString(65));
+		System.out.println("A Base64 编码后 :"
+				+ ConverterUtils.Base64Encode("A", "UTF-8"));
+		System.out.println("A Base64 解码后 :"
+				+ ConverterUtils.Base64Decode(
+						ConverterUtils.Base64Encode("A", "UTF-8"), null));
+	}
+
+}
