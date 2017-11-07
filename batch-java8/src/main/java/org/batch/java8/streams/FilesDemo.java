522ea454976e66fdd999d46a7fa3b060b90e9b3d
JKAK47 <JKAK47@users.noreply.github.com>
2017-11-07 15:19:03 +0800

    fix file finde
    fix file finde

--- batch-java8/src/main/java/org/batch/java8/streams/FilesDemo.java
+++ batch-java8/src/main/java/org/batch/java8/streams/FilesDemo.java
@@ -1,6 +1,7 @@
 package org.batch.java8.streams;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,16 +9,19 @@ import java.util.List;
 
 public class FilesDemo {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, URISyntaxException {
 		String bString = String.join(" ", "中国sdfsdf", "美fsd国dfsdfdsf", "日本");
 		System.out.println(bString);
 
 		System.out.println();
-		Path path1 = Paths.get("资源汇总.md");
+		
+		/**
+		 * 读取classpath目录下的资源文件  使用ClassLoader.getSystemResource 方法获取 。
+		 */
+		Path path1 = Paths.get(ClassLoader.getSystemResource("资源汇总.md").toURI());
 		List<String> content = Files.readAllLines(path1);
 		for (String string : content) {
 			System.out.println(string);
-
 		}
 	}
 }
