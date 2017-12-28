55b7ab09ffb8e01547c97d01fba1088fdc41651c
vincent <1557862201@qq.com>
2017-12-28 20:32:58 +0800

    Salary utils

---
+++ batch-common/src/main/java/common/utils/SalaryUtils.java
@@ -0,0 +1,15 @@
+package common.utils;
+
+/**
+ * Salary Utility
+ */
+public class SalaryUtils {
+    /** pension rate */
+    private String PENSION_RATE="pension";
+    /** MEDICAL INSURANCE RATE */
+    private String MEDICAL_INSURANCE_RATE="pension";
+    /** UNEMPLOYMENT INSURANCE RATE */
+    private String UNEMPLOYMENT_INSURANCE_RATE="pension";
+    /** COMMON RESERVE FUNDS RATE */
+    private String COMMON_RESERVE_FUNDS_RATE="pension";
+}
